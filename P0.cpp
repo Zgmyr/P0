5833e0fcdc30b05b9d8ac95e4b2c8b454026a425
Zgmyr <zacharygmyr@gmail.com>
2026-09-15 01:32:54 -0500

    started working on command line arguments & file reading

--- P0.cpp
+++ P0.cpp
@@ -4,11 +4,34 @@
 
 using namespace std;
 
+static void readStdInput();
 
 int main(int argc, char* argv[]) {
 
+	ifstream inputFile;
+
 	// process arguments & set up read file
-	
+	if (argc == 1) {
+		// no args -> read from standard input
+		readStdInput();
+		inputFile.open("temp.fs26s2");
+	}
+	else {
+		// read from file argument
+		string filename = string(argv[1]) + ".fs26s2";
+
+		inputFile.open(filename);
+
+		if (!inputFile) {
+			cout << "Failed to open file " << filename << endl;
+			return 1;
+		}
+	}
+
+	string testStr;
+	getline(inputFile, testStr);
+
+	cout << "first line read = " << testStr << endl;
 
 	// build the tree
 	
@@ -17,4 +40,13 @@ int main(int argc, char* argv[]) {
 
 
 	return 0;
+}
+
+static void readStdInput() {
+	ofstream tempFile("temp.fs26s2");
+
+	string line;
+	while (getline(cin, line)) {
+		tempFile << line << "\n";
+	}
 }
