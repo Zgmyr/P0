6a644bb3d0c0d58c38e2d87728bfa701ca02c1b7
ZachG <zgk2p@umsystem.edu>
2026-09-14 23:55:38 -0500

    Initial commit

---
+++ P0.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+
+int main(int argc, char* argv[]) {
+
+	// process arguments & set up read file
+	
+
+	// build the tree
+	
+
+	// traverse the tree 3 ways
+
+
+	return 0;
+}
